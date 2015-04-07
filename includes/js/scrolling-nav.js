e6d9b5a88e155553a6705275a4a39f539c91d6f0
Rene Gens <renegens@gmail.com>
2015-04-07 03:22:39 +0300

    Carousel, Scrolling Fix

--- includes/js/scrolling-nav.js
+++ includes/js/scrolling-nav.js
@@ -1,11 +1,11 @@
 //jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+/*$(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-});
+});*/
 
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -13,7 +13,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top -140
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
