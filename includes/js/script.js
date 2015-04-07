85bf4a79d36d0431ea14eab9e09679ca2e2b8312
Rene Gens <renegens@gmail.com>
2015-04-07 23:47:02 +0300

    Bug Fixes White menu
    Project ready, fixed div bug and menu scroll

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,10 +1,4 @@
-/*
-
-My Custom JS
-============
-
-Author:  Brad Hussey
-Updated: August 2013
-Notes:	 Hand coded for Udemy.com
-
-*/
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
